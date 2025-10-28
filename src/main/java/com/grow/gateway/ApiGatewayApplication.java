c6269b4b40bfad00883339ea6f108eeda0d9b47e
sun <suunn001@gmail.com>
2025-10-28 17:55:04 +0900

    📝 cicd: argoCD 를 이용하기 위해 CI 변경

--- src/main/java/com/grow/gateway/ApiGatewayApplication.java
+++ src/main/java/com/grow/gateway/ApiGatewayApplication.java
@@ -2,7 +2,9 @@ package com.grow.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties
 @SpringBootApplication
 public class ApiGatewayApplication {
 
